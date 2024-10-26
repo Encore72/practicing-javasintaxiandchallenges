38414812f905db77ed9d72d4311d4d90de935782
Encore72 <isaacimf@outlook.es>
2024-10-26 20:59:31 +0200

    ProyectoOrdenCompra

---
+++ 02 - Java Katas/src/ProyectoOrdenCompra/EjemploOrdenesMain.java
@@ -0,0 +1,59 @@
+package ProyectoOrdenCompra;
+
+import java.util.Date;
+
+public class EjemploOrdenesMain {
+
+    public static void main(String[] args) {
+
+        OrdenCompra orden1 = new OrdenCompra("Compra número 1");
+        orden1.setCliente(new Cliente("Jaime", "Perez"));
+        orden1.setFecha(new Date());
+
+        orden1.addProducto(new Producto("Western Digital", "Unidad SSD 480GB", 120.12));
+        orden1.addProducto(new Producto("MSI", "Monitor Optix G271", 340.99));
+        orden1.addProducto(new Producto("AMD", "Procesador AMD Ryzen 9 5900X", 349.55));
+        orden1.addProducto(new Producto("EVGA", "Tarjeta de video EVGA RTX 3080Ti", 1299.99));
+
+        OrdenCompra orden2 = new OrdenCompra("Compra número 2");
+        orden2.setCliente(new Cliente("Julio", "Rodriguez"));
+        orden2.setFecha(new Date());
+
+        orden2.addProducto(new Producto("Fensa", "Cocina", 399.81));
+        orden2.addProducto(new Producto("Samsung", "Refrigerador", 999.72));
+        orden2.addProducto(new Producto("Midea", "Lavadora", 690.25));
+        orden2.addProducto(new Producto("Aspiradora", "IRobot", 190.13));
+
+        OrdenCompra orden3 = new OrdenCompra("Compra número 3");
+        orden3.setCliente(new Cliente("Francisco", "Fernandez"));
+        orden3.setFecha(new Date());
+
+        orden3.addProducto(new Producto("Melon", "Cemento 25kg", 870.55));
+        orden3.addProducto(new Producto("Grau", "Hormigon", 6040.44));
+        orden3.addProducto(new Producto("Ceramicas Santiago", "Ladrillo", 90.44));
+        orden3.addProducto(new Producto("Redline", "Escala articulada aluminio", 90.66));
+
+
+        OrdenCompra[] ordenes = {orden1, orden2, orden3};
+
+        for (OrdenCompra orden : ordenes) {
+
+            System.out.println("Id: " + orden.getId());
+            System.out.println("Cliente: " + orden.getCliente());
+            System.out.println("Descripción: " + orden.getDescription());
+            System.out.println("Fecha: " + orden.getFecha());
+            System.out.println("Precio Total: €" + orden.getPrecioTotal());
+
+            int i = 1;
+            for (Producto p : orden.getProductos()) {
+                System.out.println("Producto " + i + ": " + p.getNombre() + " " + p.getFabricante() + " precio €" + p.getPrecio());
+                i++;
+            }
+            System.out.println("-------------------------------------------------------");
+
+        }
+
+    }
+}
+
+
---
+++ 02 - Java Katas/src/ProyectoOrdenCompra/OrdenCompra.java
@@ -0,0 +1,78 @@
+package ProyectoOrdenCompra;
+
+import java.util.Date;
+
+public class OrdenCompra {
+
+    // atributos
+    private int id;
+    private String description;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos = new Producto[4];
+
+    private static int ultimoId;
+    private int indiceProductos;
+
+
+    // constructor de solo descripción
+    public OrdenCompra(String description) {
+        this.description = description;
+        this.id = ++ultimoId;
+        this.productos = new Producto[4];
+    }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Producto[] getProductos() {
+        return productos;
+    }
+
+
+    // setters
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    // metodos
+
+        // metodo añadir productos
+    public void addProducto(Producto producto) {
+        if (indiceProductos < this.productos.length) {
+            this.productos[indiceProductos++] = producto;
+        }
+    }
+
+        // metodo obtener precio total
+    public double getPrecioTotal() {
+        double precioTotal = 0.0;
+        for (Producto p: productos) {
+            precioTotal += p.getPrecio();
+            precioTotal = Math.round(precioTotal * 100.0) / 100.0;
+        }
+        return precioTotal;
+    }
+
+
+}
+
+
